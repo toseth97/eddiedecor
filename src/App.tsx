b040668b921099422660a2ae65575e3f37c7246a
toseth97 <oluwasheun9721@gmail.com>
2024-01-10 10:14:49 +0100

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,30 @@ import "./App.css";
 import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+    const [menu, setMenu] = useState(false);
     return (
-        <div className="w-full default-bg min-h-screen">
+        <div className="w-full default__bg min-h-screen">
             <BrowserRouter>
-                <div className="w-full flex gap-5">
-                    <div className="w-3/12 p-4 ">
+                <div className="w-full flex gap-5 z-20">
+                    <div className="lg:w-2/12 p-4 lg:block hidden shadow-2xl h-screen ">
                         <Navigation />
+                    </div>
+                    <div
+                        className={`w-10/12 lg:hidden block mobile__menu p-4 shadow-2xl h-screen ${
+                            menu === true ? "active" : null
+                        } default__bg`}
+                    >
+                        <Navigation />
+                    </div>
+                    <div onClick={() => setMenu((prev) => !prev)}>
+                        {menu === false ? (
+                            <i className="bx menu__btn lg:hidden block text-4xl bx-menu-alt-right border p-2 rounded-lg"></i>
+                        ) : (
+                            <i className="bx menu__btn lg:hidden block text-4xl bx-x border p-2 rounded-lg"></i>
+                        )}
                     </div>
                     <Routes>
                         <Route path="/" element={<Hero />} />
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,53 @@
-import Logo from "../assets/Eddydecorations_logo.jpg";
+import { Link } from "react-router-dom";
+import Logo from "../assets/Eddydecorations_logo-removebg-preview.png";
 
 const Navigation = () => {
     return (
-        <header className="flex flex-col items-center justify-center">
-            <nav className="">
-                <img src={Logo} alt="logo" width={150} />
+        <header className="flex flex-col items-center py-8 w-full">
+            <nav className="flex flex-col items-center justify-center w-full">
+                <img src={Logo} alt="logo" width={150} className="logo" />
+                <ul className="w-full flex flex-col items-center justify-center default__text">
+                    <li>
+                        <Link
+                            className="px-8 py-4 inline-block cursor-pointer hover:translate-x-4"
+                            to=""
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className="px-8 py-4 inline-block cursor-pointer hover:translate-x-4"
+                            to=""
+                        >
+                            Service
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className="px-8 py-4 inline-block cursor-pointer hover:translate-x-4"
+                            to=""
+                        >
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className="px-8 py-4 inline-block cursor-pointer hover:translate-x-4"
+                            to=""
+                        >
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+                <div className="social__links"></div>
+
+                <div className="text-xs default__text text-center mt-8">
+                    <p className="text-xs default__text italic">
+                        No 1a, Titilayo cresent Olumbe Bassiar drive, Bodija,
+                        Ashi,Ibadan.
+                    </p>
+                </div>
             </nav>
         </header>
     );
