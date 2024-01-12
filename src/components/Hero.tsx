20f30ac9214be7772b767b4a19ea41284c9a8b2f
toseth97 <oluwasheun9721@gmail.com>
2024-01-12 12:15:12 +0100

    contact page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,6 +8,17 @@ import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 
 const Hero = () => {
+    const [contactMail, setContactMail] = useState({
+        name: "",
+        sender_email: "",
+        message: "",
+    });
+    const [flashMessage, setFlashMessage] = useState({
+        type: false,
+    });
+
+    const [loadState, setLoadState] = useState(false);
+
     const people = testimony;
     const [index, setIndex] = useState(0);
 
@@ -29,6 +40,62 @@ const Hero = () => {
             clearInterval(slider);
         };
     }, [index]);
+
+    //function to handle change for the react form
+    const handleInputChanges = (
+        event:
+            | React.ChangeEvent<HTMLInputElement>
+            | React.ChangeEvent<HTMLTextAreaElement>
+    ) => {
+        const { name, value } = event.target;
+        setContactMail((prev) => {
+            return { ...prev, [name]: value };
+        });
+    };
+
+    //send email
+    const sendMessage = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        setLoadState(true);
+        try {
+            const response = await fetch(
+                "https://calm-teal-cougar-vest.cyclic.app/heddy-contact",
+                {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(contactMail),
+                }
+            );
+            const data = await response.json();
+            setFlashMessage(data);
+            if (response.status === 200 && data.message) {
+                setContactMail({
+                    name: "",
+                    sender_email: "",
+                    message: "",
+                });
+                setTimeout(() => {
+                    setFlashMessage({
+                        type: false,
+                    });
+                }, 4000);
+            } else {
+                alert(
+                    "Error, We are unable to send your message. Please try again later"
+                );
+                setTimeout(() => {
+                    setFlashMessage({
+                        type: false,
+                    });
+                }, 4000);
+            }
+            setLoadState(false);
+        } catch (err) {
+            setLoadState(false);
+            console.log(err);
+        }
+    };
 
     return (
         <div className="w-full flex flex-col items-center">
@@ -250,8 +317,96 @@ const Hero = () => {
 
                     <div
                         id="contact"
-                        className="w-full flex flex-col items-center justify-center"
-                    ></div>
+                        className="w-full flex flex-col items-center justify-center dark__bg rounded-lg light__text py-8"
+                    >
+                        <div className="">
+                            <h1 className="lg:text-3xl text-2xl font-bold uppercase">
+                                Make your bookings!
+                            </h1>
+                            <div>
+                                <div>
+                                    <form onSubmit={sendMessage} method="post">
+                                        <div className="flex flex-col gap-2 my-3">
+                                            <label
+                                                htmlFor="name"
+                                                className="font-semibold"
+                                            >
+                                                Name
+                                            </label>
+                                            <input
+                                                type="text"
+                                                id="name"
+                                                name="name"
+                                                value={contactMail.name}
+                                                onChange={(event) =>
+                                                    handleInputChanges(event)
+                                                }
+                                                required
+                                                className="default__text focus:outline-none focus:border-none active:outline-none active:border-none bg-gray-200 text-sm  py-2 px-8"
+                                                placeholder="John Doe"
+                                            />
+                                        </div>
+                                        <div className="flex flex-col gap-2 my-3">
+                                            <label
+                                                htmlFor="sender_email"
+                                                className="font-semibold"
+                                            >
+                                                Email Address
+                                            </label>
+                                            <input
+                                                type="email"
+                                                id="sender_email"
+                                                name="sender_email"
+                                                value={contactMail.sender_email}
+                                                onChange={(event) =>
+                                                    handleInputChanges(event)
+                                                }
+                                                required
+                                                className=" default__text focus:outline-none focus:border-none active:outline-none active:border-none bg-gray-200 text-sm  py-2 px-8"
+                                                placeholder="email@example.com"
+                                            />
+                                        </div>
+                                        <div className="flex flex-col gap-2 my-3">
+                                            <label
+                                                htmlFor="message"
+                                                className="font-semibold"
+                                            >
+                                                Message
+                                            </label>
+                                            <textarea
+                                                id="message"
+                                                name="message"
+                                                value={contactMail.message}
+                                                onChange={(event) =>
+                                                    handleInputChanges(event)
+                                                }
+                                                required
+                                                className="default__text focus:outline-none focus:border-none active:outline-none active:border-none bg-gray-200 text-sm  py-2 px-8 h-48"
+                                                placeholder="How can i help you?"
+                                                style={{ resize: "none" }}
+                                            ></textarea>
+                                        </div>
+                                        <button className="blogBtn lg:w-5/12 w-full  flex items-center justify-center p-2">
+                                            {loadState ? (
+                                                <div className="loadState"></div>
+                                            ) : (
+                                                "Send Message"
+                                            )}
+                                        </button>
+                                        {flashMessage && (
+                                            <div
+                                                className={`text-center my-2 p-2 rounded flashMessage_${flashMessage.type}`}
+                                            >
+                                                {flashMessage.type === true
+                                                    ? "Message sent"
+                                                    : null}
+                                            </div>
+                                        )}
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
